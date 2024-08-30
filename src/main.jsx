28235efeca2a56cf3e4c3d414cdbfa26e76d4ff0
sodol-dotcom <sojeong0521@gmail.com>
2024-08-30 22:54:00 +0900

    📦️ add: inclue missing App.jsx file (추가: 누락된 App.jsx 파일 추가)
    - Added the missing App.jsx file to the project.
    (프로젝트에 누락된 App.jsx 파일을 추가)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")), render(<App />);
+// 'createRoot'로 root를 생성하고, 'render' 메서드를 호출하여 'App.jsx' 컴포넌트를 렌더링한다.
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
